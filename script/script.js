cfb802d624e4bbb2c602cc1aafb23a9520598064
Grallets <granitlleshi95@hotmail.com>
2022-09-22 17:27:30 +0200

    uppgraderad verison

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
 var form = document.getElementById('form')
 
 form.addEventListener('submit',function(event){
-event.preventDefault()
 var name= document.getElementById('name').value
-console.log(name)
 var email = document.getElementById('email').value
-console.log(email)
+alert("Hello " + name + ", You are now subscribed to our newsletter!")
+
 })
