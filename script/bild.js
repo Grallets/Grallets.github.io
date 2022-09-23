a1a3579b95a66365b352999485ac06b817e36763
Grallets <granitlleshi95@hotmail.com>
2022-09-23 13:02:36 +0200

    senaste vers

--- script/bild.js
+++ script/bild.js
@@ -1,7 +1,10 @@
-let slideIndex = 1;
+
+
+let slideIndex = 1;   //index av bilden i slideshow till 1
 showSlides(slideIndex);
 
-// Next/previous controls
+// Next/previous knapaprna -1 om man klickar 
+//på prev 1 om man klickar på next som läggs till på slideindex
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -11,15 +14,15 @@ function plusSlides(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {slideIndex = 1}      // om n blir mer än 4 så börjar den om  på 1 igen
+  if (n < 1) {slideIndex = slides.length}      // om n blir mindre än 1 så bör  4
+  for (i = 0; i < slides.length; i++) {         // går igenom alla slides och gör sått inga utav bilderna syns
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  slides[slideIndex-1].style.display = "block";  //gör display style av sliden så att endast den nuvarande bilden syns
+                                                  //detta gör det synligt
+                                                  
+                                          
+ 
 }
--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 var form = document.getElementById('form')
 
+
 form.addEventListener('submit',function(event){
-var name= document.getElementById('name').value
+
+var name= document.getElementById('name').value  
 var email = document.getElementById('email').value
 alert("Hello " + name + ", You are now subscribed to our newsletter!")
 
