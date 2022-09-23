4c12ae5d2c3771c17daa58b22e30ee8082f202e2
Grallets <granitlleshi95@hotmail.com>
2022-09-23 15:10:24 +0200

    färdig version

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ var form = document.getElementById('form')
 
 
 form.addEventListener('submit',function(event){
-
+    
 var name= document.getElementById('name').value  
 var email = document.getElementById('email').value
 alert("Hello " + name + ", You are now subscribed to our newsletter!")
